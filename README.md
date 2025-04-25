0a5cb2ad68a39ec18755c7b43bda82f58c03fb04
Fajardo <juancar18fp@gmail.com>
2025-04-25 23:35:37 +0200

    Commit inicial

---
+++ README.md
@@ -0,0 +1,2 @@
+# taller-erp
+ERP de un taller hecho con Spring y Vue para el TFG de DAM
---
+++ src/test/java/com/jcfp/tallererp/TallerErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcfp.tallererp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
